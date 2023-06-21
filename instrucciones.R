ce8d65e3216c8b52993b8d4cc193ff1ef48856c1
PaulESantos <paulefrens@gmail.com>
2023-06-20 21:07:32 -0500

    a

---
+++ instrucciones.R
@@ -0,0 +1,10 @@
+#' Para reproducir las diapositivas, necesitas tener instalado Quarto, el cual puedes
+#' descargar desde el siguiente enlace:
+#' https://quarto.org/docs/get-started/
+#'
+#' El siguiente paso es abrir el archivo index.qmd y hacer clic en el botón `render`
+#' o presionar las teclas Ctrl + Shift + K.
+#'
+#' Los archivos 00-setup.qmd ... 03-secion.qmd contienen los códigos necesarios
+#' para construir cada una de las partes de la presentación utilizadas a lo largo
+#' de las tres secciones.
